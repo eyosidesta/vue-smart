b5b6b434aea4623d0c1201229d08686bdd87f701
eyosidesta <mamesmsdesta@gmail.com>
2021-05-08 20:49:31 +0300

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <div>{{hello}} </div>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -11,6 +12,39 @@ import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
+  data: () => ({
+    books: 0,
+    hello: "yes we do"
+  }),
+  beforeCreate() {
+    console.log('beforCreate hook has been called');
+      // alert('beforCreate hook has been called');
+      console.log("books value is ", this.books)
+      
+    },
+  
+  created() {
+    console.log('books value on created is: ', this.books)
+    // alert('on create hook has been called');
+    console.log('on create hook has been called');
+  },
+
+  beforeMount() {
+    this.$data.hello = 'kakakakakakakakak'
+    // alert('beforeMount has been called')
+  },
+  mounted() {
+    this.$data.hello = 'lalalallalalalalala'
+    // alert('mounted has been called')
+  },
+  beforeUpdate() {
+    console.log('ggggggggggggggggggoooooooooooooodd before update')
+    this.$data.hello = 'goooood'
+  },
+  // updated() {
+  //   console.log('mmmmmmmmmmmmmmmmm updated')
+  //   this.$data.hello = 'mmmmmmmmm'
+  // },
   components: {
     HelloWorld
   }
